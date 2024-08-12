8484f31acf832d75bf871f44487f261e648c7272
Brad Ziegler <bradleyziegler72@gmail.com>
2024-08-12 03:01:17 -0400

    Added action priority for fighting and moving. Added a movement to a certain range when attacking. Allowed moving with a click to cancel attacking.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
+using RPG.Movement;
 
 namespace RPG.Combat {
     public class Fighter : MonoBehaviour {
-        public void Attack(CombatTarget target) {
+        [SerializeField] float weaponRange = 2f;
 
+        Transform target;
+
+        private void Update() {
+            if (target == null) { return; }
+
+            if (!IsInRange()) {
+                GetComponent<Mover>().MoveTo(target.position);
+            } else {
+                GetComponent<Mover>().Stop();
+            }
+        }
+
+        public bool IsInRange() {
+            return Vector3.Distance(transform.position, target.position) <= weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget) {
+            target = combatTarget.transform;
+        }
+
+        public void Cancel() {
+            target = null;
         }
     }
 }
