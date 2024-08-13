07c50ec8c5f7c16b264e1dfaa444ce019ce876be
Brad Ziegler <bradleyziegler72@gmail.com>
2024-08-13 02:06:20 -0400

    Imported an attack animations package and added an unarmed attack animation to our player character while in the attack state. Added an adjustable parameter to allow for delays between attacks.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -5,16 +5,28 @@ using RPG.Core;
 namespace RPG.Combat {
     public class Fighter : MonoBehaviour, IAction {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target;
+        float timeSinceLastAttack = 0f;
 
         private void Update() {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) { return; }
 
             if (!IsInRange()) {
                 GetComponent<Mover>().MoveTo(target.position);
             } else {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour() {
+            if (timeSinceLastAttack > timeBetweenAttacks) {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0f;
             }
         }
 
@@ -29,6 +41,11 @@ namespace RPG.Combat {
 
         public void Cancel() {
             target = null;
+        }
+
+        // Animation Event
+        void Hit() {
+
         }
     }
 }
