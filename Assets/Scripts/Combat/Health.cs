1d6ecc419ec81d247b65b143351de32cbc1d1cf7
Brad Ziegler <bradleyziegler72@gmail.com>
2024-08-14 01:50:55 -0400

    Added a death animation to the character animator and coded the animation to play when the characters health first reaches 0.

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 namespace RPG.Combat {
     public class Health : MonoBehaviour {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
+
+        bool isDead = false;
 
         public void TakeDamage(float damage) {
-            health = Mathf.Max(health - damage, 0f);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0f);
+            if (healthPoints == 0 && !isDead) {
+                Die();
+            }
+        }
+
+        void Die() {
+            GetComponent<Animator>().SetTrigger("die");
+            isDead = true;
         }
     }
 }
