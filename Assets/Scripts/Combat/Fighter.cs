6cdcbebd32da2c80004ebcd9c3a0025c7fa77c36
Brad Ziegler <bradleyziegler72@gmail.com>
2024-08-16 14:46:30 -0400

    - Replaced my environment with the one from the lectures so they match up to not miss anything we need to implement down the line  - Placed enemies around the level and added patrol paths to some of them  - Changed the enemy at the end of the level to be a boss enemy denoted by a different color who has more health than the rest  - Made it so that enemies move slowly when in patrol mode but then run at their full speed when in attack mode

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -18,7 +18,7 @@ namespace RPG.Combat {
             if (target.IsDead()) { return; }
 
             if (!IsInRange()) {
-                GetComponent<Mover>().MoveTo(target.transform.position);
+                GetComponent<Mover>().MoveTo(target.transform.position, 1f);
             } else {
                 GetComponent<Mover>().Cancel();
                 AttackBehaviour();
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -39,7 +39,7 @@ namespace RPG.Control {
             bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit) {
                 if (Input.GetMouseButton(0)) {
-                    GetComponent<Mover>().StartMoveAction(hit.point);
+                    GetComponent<Mover>().StartMoveAction(hit.point, 1f);
                 }
                 return true;
             }
