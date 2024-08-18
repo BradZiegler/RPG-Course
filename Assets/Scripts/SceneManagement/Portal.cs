e94f69da05264ff77fc65126df81d89eca0dc475
Brad Ziegler <bradleyziegler72@gmail.com>
2024-08-18 00:56:28 -0400

    - Added spawn points to the portals to place the player at when loading the scenes  - Added enums to the portals to have them line up which portal links to which when multiple portals exist in the scene  - Added a fader to fade the screen to black and fade back in when transitioning between scenes

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,14 +1,64 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement {
     public class Portal : MonoBehaviour {
+        enum DestinationIdentifier {
+            A, B, C, D, E, F
+        }
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeInTime = 2f;
+        [SerializeField] float fadeWaitTime = 0.5f;
 
         private void OnTriggerEnter(Collider other) {
             if (other.tag == "Player") {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition() {
+            if (sceneToLoad < 0) {
+                Debug.LogError("Scene to load not set.");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            Fader fader = FindFirstObjectByType<Fader>();
+
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+            
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal) {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal() {
+            foreach (Portal portal in FindObjectsByType<Portal>(FindObjectsSortMode.None)) {
+                if (portal == this) { continue; }
+                if (portal.destination != destination) { continue; }
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
