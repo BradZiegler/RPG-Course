9a353e794640f300e9aa0540b34112a594fd77d1
Brad Ziegler <bradleyziegler72@gmail.com>
2024-08-20 01:01:17 -0400

    - Added GUIDs to the saveableEntities class for instances of the gameobjects within scenes but not for the prefabs

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,18 +1,36 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace RPG.Saving {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour{
+        [SerializeField] string uniqueIdentifier = "";
+
         public string GetUniqueIdentifier() {
-            return "";
+            return uniqueIdentifier;
         }
 
         public object CaptureState() {
-
             return null;
         }
 
         public void RestoreState(object state) {
-            
+
         }
+
+#if UNITY_EDITOR
+        private void Update() {
+            if (Application.IsPlaying(gameObject)) { return; }
+            if (string.IsNullOrEmpty(gameObject.scene.path)) { return; }
+
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
+
+            if (string.IsNullOrEmpty(property.stringValue)) {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+#endif
     }
 }
