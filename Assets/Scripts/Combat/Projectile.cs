66475c35d9ed42d0bad2b14261b3f99ead9b5111
Brad Ziegler <bradleyziegler72@gmail.com>
2024-08-24 02:39:21 -0400

    - Added trails to the arrows and made projectile prefabs and prefab variants  - Added a bool to allow a projectile to either be homing or not and fixed some bugs related to this  - Added a fireball projectile and particle effects to both it and the impacts of arrows

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 namespace RPG.Combat {
     public class Projectile : MonoBehaviour {
         [SerializeField] float speed = 1f;
+        [SerializeField] bool isHoming = false;
+        [SerializeField] GameObject hitEffect = null;
 
         Health target = null;
         float damage = 0f;
 
-        void Update() {
+        private void Start() {
+            transform.LookAt(GetAimLocation());
+        }
+
+        private void Update() {
             if (target == null) { return; }
 
-            transform.LookAt(GetAimLocation());
+            if (isHoming && !target.IsDead()) { 
+                transform.LookAt(GetAimLocation());
+            }
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
@@ -28,8 +36,12 @@ namespace RPG.Combat {
 
         private void OnTriggerEnter(Collider other) {
             if (other.GetComponent<Health>() != target) { return; }
+            if (target.IsDead()) { return; }
 
             target.TakeDamage(damage);
+            if (hitEffect != null) {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            }
             Destroy(gameObject);
         }
     }
