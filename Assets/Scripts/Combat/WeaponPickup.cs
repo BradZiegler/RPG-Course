a6295dc65e61f08611f01ab40a2e973c0d08b56a
Brad Ziegler <bradleyziegler72@gmail.com>
2024-08-28 17:23:48 -0400

    - Added different configurable cursors depending on the available action hovered over  - Added in IRaycastable and raycast sorting  - Added in damage above characters heads

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,16 +1,22 @@
 using System.Collections;
+using RPG.Control;
 using UnityEngine;
 
 namespace RPG.Combat {
-    public class WeaponPickup : MonoBehaviour{
+    public class WeaponPickup : MonoBehaviour, IRaycastable {
         [SerializeField] Weapon weapon = null;
         [SerializeField] float respawnTime = 5f;
 
         void OnTriggerEnter(Collider other) {
-            if (other.tag == "Player") {
-                other.GetComponent<Fighter>().EquipWeapon(weapon);
-                StartCoroutine(HideForSeconds(respawnTime));
+            if (other.tag == "Player")
+            {
+                Pickup(other.GetComponent<Fighter>());
             }
+        }
+
+        private void Pickup(Fighter fighter) {
+            fighter.EquipWeapon(weapon);
+            StartCoroutine(HideForSeconds(respawnTime));
         }
 
         private IEnumerator HideForSeconds(float seconds) {
@@ -24,6 +30,17 @@ namespace RPG.Combat {
             foreach (Transform child in transform) {
                 child.gameObject.SetActive(shouldShow);
             }
+        }
+
+        public bool HandleRaycast(PlayerController callingController) {
+            if (Input.GetMouseButtonDown(0)) {
+                Pickup(callingController.GetComponent<Fighter>());
+            }
+            return true;
+        }
+
+        public CursorType GetCursorType() {
+            return CursorType.Pickup;
         }
     }
 }
